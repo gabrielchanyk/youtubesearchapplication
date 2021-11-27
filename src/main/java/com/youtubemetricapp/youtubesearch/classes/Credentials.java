8237f9779303e37912eace3f483697691ecafb81
Gabriel Chan <gabriel.chanyk@gmail.com>
2021-11-27 18:12:54 -0500

    deploy

---
+++ src/main/java/com/youtubemetricapp/youtubesearch/classes/Credentials.java
@@ -0,0 +1,13 @@
+package com.youtubemetricapp.youtubesearch.classes;
+
+//singleton class to keep secret credentials, should be in .gitignore
+public class Credentials {
+    private Credentials() {}
+
+    static public String apikey ="AIzaSyAvy1hDyi3AWLmpKeB7_3s5tqhpvLWySCU";
+//    static public String connUser = "guest";
+//    static public String connPw = "guest";
+    //heroku username and pw
+//    static public String connUser = "wgrkxzlx";
+//    static public String connPw = "3Z_cKVg7KkVFJjo88UKcuIWDNTuDcBR0";
+}
