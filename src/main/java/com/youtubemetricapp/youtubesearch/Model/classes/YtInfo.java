7b3ef601519376cede986e531703dc3a6600231c
Gabriel Chan <gabriel.chanyk@gmail.com>
2021-11-27 18:26:05 -0500

    deploy

---
+++ src/main/java/com/youtubemetricapp/youtubesearch/Model/classes/YtInfo.java
@@ -0,0 +1,29 @@
+package com.youtubemetricapp.youtubesearch.Model.classes;
+
+public class YtInfo {
+    private String url;
+    private String title;
+
+    public YtInfo(String url, String title) {
+        this.url = url;
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
